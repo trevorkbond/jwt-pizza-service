88de5d46e583cfd533b62ca3e01b2db6ce7a4211
Trevor Bond <trevorkbond@gmail.com>
2024-09-26 16:24:39 -0600

    add first auth test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,34 @@
-test('hello world', () => {
-    expect('hello' + ' world').toBe('hello world');
-  });
+const request = require("supertest");
+const app = require("../service");
+const { DB } = require("./database/database.js");
+
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUserAuthToken;
+
+async function createAdminUser() {
+  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + "@admin.com";
+
+  await DB.addUser(user);
+  user.password = "toomanysecrets";
+
+  return user;
+}
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test("login", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
+  );
+
+  const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
