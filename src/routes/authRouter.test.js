15a8156d2522425de0c5e45315cc46a322ba3f09
Trevor Bond <trevorkbond@gmail.com>
2024-09-30 13:53:48 -0600

    factor out registering functionality in authTest

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,38 +1,76 @@
 const request = require("supertest");
 const app = require("../service");
-// const { DB } = require("./database/database.js");
+const { DB, Role } = require("../database/database.js");
 
-const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
-let testUserAuthToken;
+const dinerUser = {
+  name: "pizza diner",
+  email: "reg@test.com",
+  password: "a",
+  roles: [{ role: Role.Diner }],
+};
+const adminUser = {
+  name: "pizza diner",
+  email: "reg@test.com",
+  password: "a",
+  roles: [{ role: Role.Admin }],
+};
+let dinerUserAuthToken;
+let adminUserAuthToken;
+let franchiseeUserAuthToken;
 
-// async function createAdminUser() {
-//   let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
-//   user.name = randomName();
-//   user.email = user.name + "@admin.com";
+function setLocalAuthToken(user, authToken) {
+  const userRole = user.roles[0].role;
+  switch (userRole) {
+    case Role.Admin:
+      adminUserAuthToken = authToken;
+      break;
+    case Role.Diner:
+      dinerUserAuthToken = authToken;
+      break;
+    case Role.Franchisee:
+      franchiseeUserAuthToken = authToken;
+      break;
+  }
+}
 
-//   await DB.addUser(user);
-//   user.password = "toomanysecrets";
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
 
-//   return user;
-// }
-
-beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
-  const registerRes = await request(app).post("/api/auth").send(testUser);
-  testUserAuthToken = registerRes.body.token;
-  expect(testUserAuthToken).toMatch(
+async function registerUser(user) {
+  user.email = randomName() + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(user);
+  authToken = registerRes.body.token;
+  setLocalAuthToken(user, authToken);
+  expect(authToken).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
   );
-});
+}
 
-test("login", async () => {
-  const loginRes = await request(app).put("/api/auth").send(testUser);
+async function loginUser(user) {
+  const loginRes = await request(app).put("/api/auth").send(user);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
   );
+  return loginRes;
+}
 
-  const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
+beforeAll(async () => {
+  await registerUser(dinerUser);
+  await registerUser(adminUser);
+});
+
+test("Login with diner user", async () => {
+  const loginRes = await loginUser(dinerUser);
+  const { password, ...user } = { ...dinerUser, roles: [{ role: "diner" }] };
   expect(loginRes.body.user).toMatchObject(user);
-  expect(password).toBeTruthy();
+  expect(password).toBe("a");
+});
+
+test("Login with admin user", async () => {
+  const loginRes = await loginUser(adminUser);
+  const { password, ...user } = { ...adminUser, roles: [{ role: "admin" }] };
+  expect(loginRes.body.user).toMatchObject(user);
+  expect(password).toBe("a");
 });
