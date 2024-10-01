d017d10c7008ff3a03e2b3a3ff13b1dfb8c5492a
Trevor Bond <trevorkbond@gmail.com>
2024-09-30 20:10:37 -0600

    add createUser function

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,19 +4,37 @@ const { DB, Role } = require("../database/database.js");
 
 const dinerUser = {
   name: "pizza diner",
-  email: "reg@test.com",
   password: "a",
   roles: [{ role: Role.Diner }],
 };
 const adminUser = {
-  name: "pizza diner",
-  email: "reg@test.com",
+  name: "pizza admin",
   password: "a",
   roles: [{ role: Role.Admin }],
 };
+const franchiseeUser = {
+  name: "pizza franchisee",
+  password: "a",
+  roles: [{ role: Role.Franchisee }],
+};
+const testUsers = {
+  admin: adminUser,
+  diner: dinerUser,
+  franchisee: franchiseeUser,
+};
+
 let dinerUserAuthToken;
 let adminUserAuthToken;
 let franchiseeUserAuthToken;
+
+async function createUser(user) {
+  user.email = randomName() + "@admin.com";
+
+  await DB.addUser(user);
+  user.password = "a";
+
+  return user;
+}
 
 function setLocalAuthToken(user, authToken) {
   const userRole = user.roles[0].role;
@@ -37,16 +55,6 @@ function randomName() {
   return Math.random().toString(36).substring(2, 12);
 }
 
-async function registerUser(user) {
-  user.email = randomName() + "@test.com";
-  const registerRes = await request(app).post("/api/auth").send(user);
-  authToken = registerRes.body.token;
-  setLocalAuthToken(user, authToken);
-  expect(authToken).toMatch(
-    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
-  );
-}
-
 async function loginUser(user) {
   const loginRes = await request(app).put("/api/auth").send(user);
   expect(loginRes.status).toBe(200);
@@ -57,8 +65,12 @@ async function loginUser(user) {
 }
 
 beforeAll(async () => {
-  await registerUser(dinerUser);
-  await registerUser(adminUser);
+  Object.entries(testUsers).forEach(async ([role, user]) => {
+    console.log(user);
+    const createdUser = await createUser(user);
+    testUsers.role = createdUser;
+  });
+  console.log(testUsers);
 });
 
 test("Login with diner user", async () => {
