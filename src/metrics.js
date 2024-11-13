282b7e12d9e0e8c21753aef48ad9ba78c22366c5
Trevor Bond <trevorkbond@gmail.com>
2024-11-12 22:16:09 -0700

    Finish up metrics

--- src/metrics.js
+++ src/metrics.js
@@ -12,8 +12,39 @@ class Metrics {
     this.apiKey = config.apiKey;
     this.userId = config.userId;
     this.metricsUrl = config.url;
+    this.numActiveUsers = 0;
+    this.failedLogins = 0;
+    this.successfulLogins = 0;
+    this.revenue = 0;
+    this.successfulOrders = 0;
+    this.failedOrders = 0;
 
+    this.requestDurations = [];
+    this.pizzaCreationDurations = [];
+    this.maxDurationsToKeep = 100;
+
+    this.requestTracker = this.requestTracker.bind(this);
     this.sendMetricsPeriodically(10000);
+  }
+
+  recordRequestDuration(duration) {
+    this.requestDurations.push(duration);
+    if (this.requestDurations.length > this.maxDurationsToKeep) {
+      this.requestDurations.shift();
+    }
+  }
+
+  recordPizzaDuration(duration) {
+    this.pizzaCreationDurations.push(duration);
+    if (this.pizzaCreationDurations.length > this.maxDurationsToKeep) {
+      this.pizzaCreationDurations.shift();
+    }
+  }
+
+  getAverageDuration(durations) {
+    if (!durations || durations.length === 0) return 0;
+    const sum = durations.reduce((acc, curr) => acc + curr, 0);
+    return sum / durations.length;
   }
 
   sendMetricToGrafana(metric) {
@@ -26,8 +57,6 @@ class Metrics {
         if (!response.ok) {
           console.error("Failed to push metrics data to Grafana");
           console.log(response);
-        } else {
-          console.log(`Pushed ${metric}`);
         }
       })
       .catch((error) => {
@@ -65,15 +94,123 @@ class Metrics {
     return memoryUsage.toFixed(2);
   }
 
+  recordLogin() {
+    this.successfulLogins++;
+    this.numActiveUsers++;
+  }
+
+  incrementFailedLogins() {
+    this.failedLogins++;
+  }
+
+  recordLogout() {
+    this.numActiveUsers--;
+  }
+
+  recordSuccessfulOrder(price) {
+    this.revenue += price;
+    this.successfulOrders++;
+  }
+
+  recordFailedOrder() {
+    this.failedOrders++;
+  }
+
+  requestTracker(req, res, next) {
+    this.totalRequests++;
+
+    switch (req.method) {
+      case "GET":
+        this.numGetRequests++;
+        break;
+      case "POST":
+        this.numPostRequests++;
+        break;
+      case "PUT":
+        this.numPutRequests++;
+        break;
+      case "DELETE":
+        this.numDeleteRequests++;
+        break;
+      default:
+        break;
+    }
+
+    const startTime = process.hrtime();
+
+    res.on("finish", () => {
+      const hrDuration = process.hrtime(startTime);
+      const durationInMs = hrDuration[0] * 1000 + hrDuration[1] / 1000000;
+      this.recordRequestDuration(durationInMs);
+    });
+
+    next();
+  }
+
+  httpMetrics(buf) {
+    buf.addMetric("request", "method", "GET", "get", this.numGetRequests);
+    buf.addMetric("request", "method", "POST", "post", this.numPostRequests);
+    buf.addMetric("request", "method", "PUT", "put", this.numPutRequests);
+    buf.addMetric(
+      "request",
+      "method",
+      "DELETE",
+      "delete",
+      this.numDeleteRequests
+    );
+    buf.addMetric("request", "total", "all", "total", this.totalRequests);
+    buf.addMetric(
+      "request",
+      "duration",
+      "avg",
+      "avg_duration",
+      this.getAverageDuration(this.requestDurations)
+    );
+    buf.addMetric(
+      "request",
+      "duration",
+      "avg",
+      "pizza_duration",
+      this.getAverageDuration(this.pizzaCreationDurations)
+    );
+  }
+
+  userMetrics(buf) {
+    buf.addMetric("users", "users", "active", "active", this.numActiveUsers);
+  }
+
+  authMetrics(buf) {
+    buf.addMetric("auth", "auth", "success", "success", this.successfulLogins);
+    buf.addMetric("auth", "auth", "failed", "failed", this.failedLogins);
+  }
+
+  purchaseMetrics(buf) {
+    buf.addMetric(
+      "purchase",
+      "purchase",
+      "success",
+      "success",
+      this.successfulOrders
+    );
+    buf.addMetric(
+      "purchase",
+      "purchase",
+      "failed",
+      "failed",
+      this.failedOrders
+    );
+    buf.addMetric("purchase", "purchase", "revenue", "revenue", this.revenue);
+  }
+
   sendMetricsPeriodically(period) {
     const timer = setInterval(() => {
       try {
         const buf = new MetricBuilder();
-        // httpMetrics(buf);
+        this.httpMetrics(buf);
         this.systemMetrics(buf);
-        // userMetrics(buf);
-        // purchaseMetrics(buf);
-        // authMetrics(buf);
+        this.userMetrics(buf);
+        this.purchaseMetrics(buf);
+        this.authMetrics(buf);
 
         const metrics = buf.toString();
         this.sendMetricToGrafana(metrics);
--- src/service.js
+++ src/service.js
@@ -9,7 +9,7 @@ const metrics = require("./metrics.js");
 const app = express();
 app.use(express.json());
 app.use(setAuthUser);
-// app.use(metrics.requestTracker);
+app.use(metrics.requestTracker);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -51,6 +51,13 @@ app.use("*", (req, res) => {
 
 // Default error handler for all exceptions and errors.
 app.use((err, req, res, next) => {
+  if (
+    req.path === "/api/auth" &&
+    req.method === "PUT" &&
+    err.statusCode === 404
+  ) {
+    metrics.incrementFailedLogins();
+  }
   res
     .status(err.statusCode ?? 500)
     .json({ message: err.message, stack: err.stack });
